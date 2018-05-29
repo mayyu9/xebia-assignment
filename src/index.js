09ab8493f3a65343363184076779f1c6e54de8cd
MT049397 <mahendar.thakur@cerner.com>
2018-05-29 12:00:11 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './LoginFrom';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
